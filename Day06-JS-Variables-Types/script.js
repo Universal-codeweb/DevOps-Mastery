b47a8d51064626056eb7023dd5d5f8322207d9f2
Universal-codeweb <jvswethavenkat@gmail.com>
2026-06-22 19:34:47 +0530

    Day 6 - 22 -06-2026

---
+++ Day06-JS-Variables-Types/script.js
@@ -0,0 +1,38 @@
+let name = 'Universian';
+const $a ='Cosmic';
+//data types
+//Number
+let n1 =5;
+let n2 =6;
+console.log(n1,n2);
+//string
+let s1 ="Hello, universe!";
+let s2 ='Single quotes too works';
+console.log(s1,s2);
+//Boolean
+let isThisWork = true;
+const isThisChangable = false;
+console.log(isThisWork, isThisChangable);
+//null
+let age =null;
+console.log(null);
+//symbol
+let sy1 = Symbol("Geeks");
+let sy2 = Symbol("Geeks");
+console.log(s1==s2);
+//arrays
+const galaxies =[ Andro, milky, Tone];
+console.log(galaxies);
+//Operators
+let n3 = '5';
+console.log(n1==n3);
+console.log(n1===n3);
+console.log(n1!=n3);
+console.log(n1!==n3);
+
+console.log(n1+n2);
+console.log(n1%n2);
+console.log(n1/n2);
+console.log(n1-n2);
+console.log(n1*n2);
+
