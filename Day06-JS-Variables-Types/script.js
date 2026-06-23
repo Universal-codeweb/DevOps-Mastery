2d7de47bc4e755707907164ffc33d5fc8bd92fbe
Universal-codeweb <jvswethavenkat@gmail.com>
2026-06-23 14:16:46 +0530

    operators and expressions update

---
+++ Day06-JS-Variables-Types/script.js
@@ -0,0 +1,84 @@
+let name = 'Universian';
+const $a ='Cosmic';
+//data types
+//Number
+let n1 =5;
+let n2 =6;
+console.log(n1,n2);
+//string
+let s1 ="Hello, universe!";
+let s2 ='Single quotes too works';
+console.log(s1,s2);
+//Boolean
+let isThisWork = true;
+const isThisChangable = false;
+console.log(isThisWork, isThisChangable);
+//null
+let age =null;
+console.log(null);
+//symbol
+let sy1 = Symbol("Geeks");
+let sy2 = Symbol("Geeks");
+console.log(s1==s2);
+//arrays
+//const galaxies =[Andro, milky, Tone];
+//console.log(galaxies);
+//Operators
+//comparison operators
+let n3 = '5';
+console.log(n1==n3);
+console.log(n1===n3);
+console.log(n1!=n3);
+console.log(n1!==n3);
+//Arithmetic operators
+console.log(n1+n2);
+console.log(n1%n2);
+console.log(n1/n2);
+console.log(n1-n2);
+console.log(n1*n2);
+//assignment operators
+let a = 5
+console.log(a);
+a+=a;
+console.log(a);
+a-=a;
+console.log(a);
+a=10;
+console.log(a);
+a*=2;
+console.log(a);
+a/=a;
+console.log(a);
+a%=1;
+console.log(a);
+//Logical 
+const n =true, m= false;
+console.log(n && m);
+console.log(n||m);
+console.log(!n);
+//Unary Operators
+let x =10
+console.log(+x);
+console.log(-x);
+console.log(++x);
+console.log(--x);
+console.log(!x);
+const step1= void 0;
+console.log(step1);
+const step2= delete step1;
+console.log(step2);
+const step3 = typeof step2;
+console.log(step3);
+//BItwise
+const res = 2 & 8;
+console.log(res);
+let r = 2|5;
+console.log(r);
+r = 5<<1;
+console.log(r);
+r = 5>>2;
+//BigInt
+const big1 = 123456789012345678901234567890n;
+const big2 = 987654321098765432109876543210n;
+console.log(big1 + big2);
+//
